ab7fa2f0aaf7cb4dff4bd1518813cf971432eef1
James Sowerby <james.sowerby@northumbria.ac.uk>
2024-02-28 18:05:51 +0000

    pre processing

--- model.py
+++ model.py
@@ -1,43 +1,32 @@
 import pandas as pd
 import torch
+from sklearn.model_selection import train_test_split
 from transformers import BertTokenizer, BertForSequenceClassification
 
 # Load the dataset
 file_path = 'src/datasets/Truth_Seeker_Model_Dataset.csv'
 df = pd.read_csv(file_path)
 
-# Splitting the dataframe based on the 'target' column
-df_true = df[df['target'] == True]
-df_false = df[df['target'] == False]
+missing_values_count = df.isnull().sum() # we get the number of missing data points per column
+print("Number of missing data points per column:\n")
+print (missing_values_count)
+df.dropna(inplace=True) #drop the empty rows
 
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+train_df, sub_df = train_test_split(df, stratify=df.BinaryNumTarget.values, 
+                                                random_state=42, 
+                                                test_size=0.2, shuffle=True)
 
-# Example text
-texts = df['tweet'].tolist()  # Use the Tweet column for classification
+validation_df, test_df = train_test_split(sub_df, stratify=sub_df.BinaryNumTarget.values, 
+                                                random_state=42, 
+                                                test_size=0.25, shuffle=True)
 
-# Tokenize and prepare for BERT
-input_ids = []
-attention_masks = []
+train_df.reset_index(drop=True, inplace=True)
+validation_df.reset_index(drop=True, inplace=True)
+test_df.reset_index(drop=True, inplace=True)
 
-for text in texts:
-    encoded_dict = tokenizer.encode_plus(
-                        text,                      # Text to encode
-                        add_special_tokens = True, # Add '[CLS]' and '[SEP]'
-                        max_length = 64,           # Pad & truncate all sentences
-                        pad_to_max_length = True,
-                        return_attention_mask = True,   # Construct attention masks
-                        return_tensors = 'pt',     # Return pytorch tensors
-                )
-    
-    # Add the encoded sentence to the list    
-    input_ids.append(encoded_dict['input_ids'])
-    
-    # And its attention mask (simply differentiates padding from non-padding)
-    attention_masks.append(encoded_dict['attention_mask'])
+# We check the number of examples after split
+print("Train data: {} \n".format(train_df.shape))
+print("Validation data: {} \n".format(validation_df.shape))
+print("Test data: {} \n".format(test_df.shape))
 
-# Convert the lists into tensors
-input_ids = torch.cat(input_ids, dim=0)
-attention_masks = torch.cat(attention_masks, dim=0)
-labels = torch.tensor(df['BinaryNumTarget'].tolist())  # Assuming 'BinaryNumTarget' is your target variable
 
-# You now have `input_ids`, `attention_masks`, and `labels` for your dataset
