7ad3af5807d06c59561546eee5b20fd246258034
JimzSnowby <117113270+JimzSnowby@users.noreply.github.com>
2024-04-23 09:27:38 +0100

    Added posting
    Fetches and returns a prediction on the first 10 tweets found by query. Requires paid access to API.

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 from tweet import tweet
+import authentication
+from transformers import pipeline
 
-if __name__ == "__main__":
-    tweet("API v2 with image test", "src/dog.jpg")
+# Declare the API connection and the classifier
+client = authentication.get_x_conn_v2()
+classifier = pipeline('text-classifcation', model='src/models/FN_Truth_Seeker_Model/checkpoint-6710')
+
+# Search term for finding tweets
+query = "president -is:retweet"
+
+# Get the tweets
+response = client.search_recent_tweets(query, max_results=10)
+# For each tweet, classify it and reply to it with the prediction result
+for i in response.data:
+    pred = classifier(i.text)
+    reply = client.create_tweet(in_reply_to_tweet_id=i.id, text="Fake News Checker bot suggests this post is: " + str(pred[i]['label']) + " with a confidence of " + str(pred[i]['score']))
+
+# test tweet with image
+#tweet("API v2 with image test", "src/dog.jpg")
